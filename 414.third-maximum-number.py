893845f178ed269ed7b9d35f2cb5c9f791e536eb
mdasadul <mdasadul@ualberta.ca>
2019-09-15 13:25:53 -0400

    making the code faster

--- 414.third-maximum-number.py
+++ 414.third-maximum-number.py
@@ -1,18 +1,14 @@
 class Solution:
-    def del_max(self,nums):
-        first_max = max(nums)
-        for i in range(nums.count(first_max)):
-            nums.remove(first_max)
-        return nums
+
     def thirdMax(self, nums) :
-        first_max = max(nums)
+        
         hash = set()
         for item in nums: 
             if item not in hash:
                 hash.add(item)
-        if len(hash)<=2: return first_max
-        for i in range(2):
-            nums = self.del_max(nums)
-        return max(nums)
+        if len(hash)<=2: return max(hash)
+        hash.remove(max(hash))
+        hash.remove(max(hash))
+        return max(hash)
                     
       
