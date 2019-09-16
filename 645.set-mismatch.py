a3925bd3d51c538e3468a3ed1313b140d6dfc844
mdasadul <mdasadul@ualberta.ca>
2019-09-16 07:17:57 -0400

    adding 645 set mitchmatch

--- 645.set-mismatch.py
+++ 645.set-mismatch.py
@@ -6,15 +6,16 @@
 class Solution:
     def findErrorNums(self, nums: List[int]) -> List[int]:
         d =  collections.Counter(nums)
+        l = len(nums)
         h = []
         h=[(-v,k) for k,v in d.items()]
         heapq.heapify(h)
         ret = []
         
         v,k = heapq.heappop(h)
-        i = 1
-        for key,_ in zip(d.items():
-            if i!=key:
+
+        for i in range(1,l+1):
+            if i not in d:
                 return [k,i]
-            i +=1
+    
             
