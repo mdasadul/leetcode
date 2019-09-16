98243cb8c65e39bf3a6438049d6229e89931a226
mdasadul <mdasadul@ualberta.ca>
2019-09-15 21:59:03 -0400

    adding top k

--- 451.sort-characters-by-frequency.py
+++ 451.sort-characters-by-frequency.py
@@ -6,10 +6,14 @@
 class Solution:
     def frequencySort(self, s: str) -> str:
         s = collections.Counter(list(s))
-        t =[]
-        for k ,v in sorted(s.items(), key=lambda item:item[1], reverse = True) :
-            t +=[k]*v
-        return ''.join(t)
+        h =[]
+        for k,v in s.items():
+            heapq.heappush(h,(-v,k))
+        ret =[]
+        while h:
+            v, k = heapq.heappop(h)
+            ret +=[k]*(-v) 
+        return ''.join(ret)
         
       
 
