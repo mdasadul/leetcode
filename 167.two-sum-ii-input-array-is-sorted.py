03ee78d6822b0ae9ef45aacea6478ceae82982fc
mdasadul <mdasadul@ualberta.ca>
2019-09-19 11:09:20 -0400

    adding most container with water

--- 167.two-sum-ii-input-array-is-sorted.py
+++ 167.two-sum-ii-input-array-is-sorted.py
@@ -4,15 +4,10 @@
 # [167] Two Sum II - Input array is sorted
 #
 class Solution:
-    def binary_search(self, nums, target):
-        n = len(nums)
-        l, h = 0, n-1 
-        while(l <=h):
-            if target ==nums[l]+nums[h]:
-                return [l+1,h+1]
-            elif target > nums[l]+nums[h]:
-                l = l+1
-            else:
-                h = h - 1
+
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-            return self.binary_search(numbers, target)
+            h ={}
+            for index, item in enumerate(numbers):
+                if target-item in h:
+                    return [h[target-item]+1, index+1]
+                h[item] =index
