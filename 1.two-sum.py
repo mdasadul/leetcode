0653bf65e1cf2095e30cd1c98164942146203dad
mdasadul <mdasadul@ualberta.ca>
2019-09-23 17:04:08 -0400

    hash table

--- 1.two-sum.py
+++ 1.two-sum.py
@@ -5,21 +5,11 @@
 #
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        n = nums.copy()
-        l = 0
-        nums.sort()
-        r = len(nums)-1
-        # print(n)
-        ret = []
-        while l < r:
-            sum_ = nums[l] + nums[r]
-            # print(sum_)
-            if target ==sum_:
-               return [n.index(nums[l]),len(n)-1-n[::-1].index(nums[r])]
-            elif target > sum_:
-               l += 1
-            else:
-                r -= 1
-            
+        hash = {}
+        for index, item in enumerate(nums):
+            if target - item in hash:
+                return [hash[target-item],index]
+            hash[item] = index
+       
             
 
