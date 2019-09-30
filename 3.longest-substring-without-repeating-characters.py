5539c5da3a8b96b059007d9eb578c983572c6814
mdasadul <mdasadul@ualberta.ca>
2019-09-30 06:38:44 -0400

    longest substring

--- 3.longest-substring-without-repeating-characters.py
+++ 3.longest-substring-without-repeating-characters.py
@@ -1,22 +1,26 @@
 #
-# @lc app=leetcode id=3 lang=python3
+# @lc app=leetcode id=1143 lang=python3
 #
-# [3] Longest Substring Without Repeating Characters
+# [1143] Longest Common Subsequence
 #
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         if s=='': return 0
         max_length =1
-        
-        for i in range(len(s)):
-            sub =''
-            for item in s[i:]:
-                if item in sub:
-                    sub +=item
-                else:
-                    if max_length<len(sub):
-                        max_length = len(sub) 
-                    sub = item
-            if max_length<len(sub):max_length=len(sub)
+        d = set()
+        max_length = 0
+        length = 0
+        sub_str=''
+        for i in s:
+            if i not in d: 
+                length += 1 
+                d.add(i)
+                sub_str +=i
+            else: 
+                    sub_str = sub_str[sub_str.index(i)+1:]+i
+                    length =len(sub_str)
+                    d=set(sub_str)
+            if max_length < length:
+                    max_length = length 
+            
         return max_length
-
