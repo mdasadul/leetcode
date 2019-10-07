a98b11c35b16a9f59f77a7c13a449a5c4afefbf6
mdasadul <mdasadul@ualberta.ca>
2019-10-07 09:43:19 -0400

    climing stairs

--- 70.climbing-stairs.py
+++ 70.climbing-stairs.py
@@ -5,5 +5,5 @@
 #
 class Solution:
     def climbStairs(self, n: int) -> int:
-        return round((((math.sqrt(5) + 1)/2)** n) / math.sqrt(5))
+        return round((((math.sqrt(5) + 1)/2)** (n+1)) / math.sqrt(5))
       
