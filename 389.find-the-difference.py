331666a9a3fe46c251a83683c2702205aaa9af66
mdasadul <mdasadul@ualberta.ca>
2019-10-08 21:44:20 -0400

    find the

--- 389.find-the-difference.py
+++ 389.find-the-difference.py
@@ -5,16 +5,5 @@
 #
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
-        h_s = {}
-        for i in t:
-            if i in h_s:
-                h_s[i] +=1
-            else:
-                h_s[i] = 1
-        for item in s:
-            if item in h_s:
-                h_s[item] -=1
-                if h_s[item]==0:
-                    del h_s[item]
-        return (list(h_s.keys())[0])
+      return list(collections.Counter(t)-collections.Counter(s))[0]
             
