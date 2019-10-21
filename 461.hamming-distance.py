17312b5560c454786e8b2f9f2a841f9572039b1b
mdasadul <mdasadul@ualberta.ca>
2019-10-21 08:27:45 -0400

    adding hamming distance

---
+++ 461.hamming-distance.py
@@ -0,0 +1,13 @@
+#
+# @lc app=leetcode id=461 lang=python3
+#
+# [461] Hamming Distance
+#
+
+# @lc code=start
+class Solution:
+    def hammingDistance(self, x: int, y: int) -> int:
+        return str(bin(x^y))[2:].count('1')
+        
+# @lc code=end
+
